955b7f5b1510c5fd55ba116ab49e1da57b8ac816
Fabricio Pinheiro Mendes <120844246+fabriciopmendes@users.noreply.github.com>
2023-05-15 16:51:45 -0300

    update

--- conceitos.js
+++ conceitos.js
@@ -287,3 +287,42 @@ submitButton.addEventListener('click', function(e) {
     items.children[1].textContent = emailValue;
     body.style.background = 'white'
 })
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const submitButton = document.querySelector('#submit-button')
+
+const errorMessage = document.querySelector('.msg')
+
+const items = document.querySelector('.items')
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+
+    if (nameValue === '' || emailValue === '') {
+        errorMessage.textContent = "Please fill out the fields!"
+        errorMessage.classList = 'error';
+
+        setTimeout(() => {
+            errorMessage.textContent = '';
+            errorMessage.classList = '';
+    }, 3000);
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.classList = 'item';
+    li.innerHTML = `Nome: ${nameValue}<br />Email: ${emailValue}`
+
+    items.appendChild(li);
+
+    nameInput.value = '';
+    emailInput.value = '';
+})
+//Proximos passos
+Praticar
+Promises, Async/await
+Framework fron-end, Ex: React
+TypeScript
